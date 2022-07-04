4c8b3a582bef1226672c244940132bbf567b9a01
Feruz <59234162+Feruzteame@users.noreply.github.com>
2022-07-04 09:02:09 +0200

    fetch data using random API

--- client/src/app/components/fetch/fetch.component.ts
+++ client/src/app/components/fetch/fetch.component.ts
@@ -12,14 +12,12 @@ export class FetchComponent implements OnInit {
   constructor(private http : HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get('http://books.cloudfoundry.com/data/authors')
-    .subscribe(Response => {
- 
-      if(Response){ 
+    this.http.get('https://pokeapi.co/api/v2/pokemon?limit=50&offset=0')
+    .subscribe(res => {
+        if(res){
+        console.log(res)
       }
-      console.log(Response)
-      this.li=Response;
-      this.lis=this.li.list;
+
     });
 
   }
